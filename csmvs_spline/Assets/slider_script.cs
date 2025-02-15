4778733e9c233dba9727971dde336d4eb9e75ebe
medusashy <scdey.501@gmail.com>
2025-02-15 17:04:51 +0530

    implemented_slider

--- csmvs_spline/Assets/slider_script.cs
+++ csmvs_spline/Assets/slider_script.cs
@@ -2,19 +2,54 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Splines;
 using UnityEngine.EventSystems;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 //[ExecuteAlways]
 public class slider_script : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 
     public Slider slider;
-    public GameObject[] images;
+
+    public GameObject image_container;
+    public GameObject image_container_large;
+
+
+    public List<GameObject> images = new List<GameObject>();
+    public List<GameObject> images_large = new List<GameObject>();
+
+
     public float[] og_offset;
+    public float[] og_offset_large;
+
+
     private bool slider_pressed = false;
     public float old_slider_value;
 
     private void Start()
     {
+
+
+
+        foreach (Transform child in image_container.transform)
+        {
+            foreach (Transform child2 in child.transform)
+            {
+                images.Add(child2.gameObject);
+            }
+        }
+        foreach (Transform child in image_container_large.transform)
+        {
+            foreach (Transform child2 in child.transform)
+            {
+                images_large.Add(child2.gameObject);
+            }
+        }
+
+        
+        
+        
+        
         //og_offset = new float[images.Length];
 
         //for (int i = 0; i < images.Length; i++)
@@ -28,18 +63,32 @@ public class slider_script : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     }
 
 
+
+
+
     public void OnPointerDown(PointerEventData eventData)
     {
         old_slider_value = slider.value;
-        og_offset = new float[images.Length];
+        og_offset = new float[images.Count];
+        og_offset_large = new float[images_large.Count];
 
-        for (int i = 0; i < images.Length; i++)
+        for (int i = 0; i < images.Count; i++)
 
         {
-        og_offset[i] = images[i].GetComponent<SplineAnimate>().ElapsedTime;
+            og_offset[i] = images[i].GetComponent<SplineAnimate>().ElapsedTime;
         }
+        for (int i = 0; i < images_large.Count; i++)
+
+        {
+            og_offset_large[i] = images_large[i].GetComponent<SplineAnimate>().ElapsedTime;
+        }
+        
+        
+        
+        
+        
         slider_pressed = true;
-        Debug.Log("huh");
+       
 
     }
 
@@ -58,42 +107,43 @@ public class slider_script : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     void Update()
     {
 
-
-        Debug.Log(images[0].GetComponent<SplineAnimate>().ElapsedTime);
-        for (int i = 0; i < images.Length; i++)
-
-        {
-
-           
-            //img.GetComponent<SplineAnimate>().NormalizedTime = slider.value; // WORKS BUT BAD
-            //img.GetComponent<SplineAnimate>().StartOffset = slider.value;
-        }
-
-
-        //comment out for showcase
         if (slider_pressed)
         {
-            //Debug.Log("Slider pressed");
-            for (int i = 0; i < images.Length; i++)
-
+            for (int i = 0; i < images.Count; i++)
             {
                 images[i].GetComponent<SplineAnimate>().Pause();
-                float etTemp = images[i].GetComponent<SplineAnimate>().ElapsedTime;
-                images[i].GetComponent<SplineAnimate>().ElapsedTime = og_offset[i] + (slider.value - old_slider_value) * 30;
+                images[i].GetComponent<SplineAnimate>().ElapsedTime = og_offset[i] + (slider.value - old_slider_value) * 35;
+            }
+            
+            for (int i = 0; i < images_large.Count; i++)
+            {
+                images_large[i].GetComponent<SplineAnimate>().Pause();
+                images_large[i].GetComponent<SplineAnimate>().ElapsedTime = og_offset_large[i] + (slider.value - old_slider_value) * 46;
             }
         }
-        
+
+
+
+
+
+
         if (!slider_pressed)
         {
-            for (int i = 0; i < images.Length; i++)
+            for (int i = 0; i < images.Count; i++)
 
             {
                 images[i].GetComponent<SplineAnimate>().Play();
+                
+            }
+            for (int i = 0; i < images_large.Count; i++)
 
+            {
+                images_large[i].GetComponent<SplineAnimate>().Play();
+                
             }
         }
 
-       
+
 
 
     }
