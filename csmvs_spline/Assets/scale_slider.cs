1664f6d3e75daf04ccb60efeb31a3292e979ce83
medusashy <scdey.501@gmail.com>
2025-02-18 15:00:01 +0530

    applied_scale_and_speed_sliders

--- csmvs_spline/Assets/scale_slider.cs
+++ csmvs_spline/Assets/scale_slider.cs
@@ -5,13 +5,24 @@ public class scale_slider : MonoBehaviour
 {
     //[Range(1.0f, 5.0f)]
     public Slider scaleSlider;
+    public Slider scaleSlider_ind;
     private float scaleSliderNumber;
+    private float scaleSliderNumber_ind;
 
-    
+
+    //Slider scale control here!!!
+    private float individual_Slider_Multiplier = 6f;
+    private float global_Slider_Multiplier = 0.005f;
+
+
     void Update()
     {
-        scaleSliderNumber = scaleSlider.value * 0.0045f;
-        Vector3 scale = new Vector3(scaleSliderNumber, scaleSliderNumber, scaleSliderNumber);
+        scaleSliderNumber_ind = scaleSlider_ind.value * individual_Slider_Multiplier;
+        //Vector3 scale_ind = new Vector3(scaleSliderNumber_ind, scaleSliderNumber_ind, scaleSliderNumber_ind);
+        //this.transform.localScale = scale_ind;
+
+        scaleSliderNumber = scaleSlider.value * global_Slider_Multiplier;
+        Vector3 scale = new Vector3(scaleSliderNumber * scaleSliderNumber_ind, scaleSliderNumber * scaleSliderNumber_ind, scaleSliderNumber * scaleSliderNumber_ind);
         this.transform.localScale = scale;
     }
 }
